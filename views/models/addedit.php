30f56d5d5141bf248268a2a1d9d2dfca40a0719c
Craig A Rodway <craig.rodway@gmail.com>
2014-01-16 22:50:56 +0000

    Models: Add Another.

--- views/models/addedit.php
+++ views/models/addedit.php
@@ -4,35 +4,36 @@ $tpl->place('header');
 ?>
 
 <div class="grid_12">
-	
+
 	<form action="<?php echo fURL::get() ?>" method="post">
 	<input type="hidden" name="action" value="<?php echo $action ?>" />
 	<input type="hidden" name="id" value="<?php echo $m->getId() ?>" />
-		
+
 		<table class="form">
-		
+
 			<tr>
 				<td class="caption"><label for="manufacturer_id">Manufacturer</label></td>
 				<td class="input">
 					<select name="manufacturer_id" id="manufacturer_id">
 					<?php
 					$option = '<option value="%d">%s</option>';
+					$selected = ($action === 'add' ? fMessaging::retrieve('manufacturer_id', 'models/add') : $m->getManufacturerId());
 					foreach($manufacturers as $mfr){
-						fHTML::printOption($mfr->getName(), $mfr->getId(), $m->getManufacturerId());
+						fHTML::printOption($mfr->getName(), $mfr->getId(), $selected);
 					}
 					?>
 					</select>
 				</td>
 			</tr>
-			
+
 			<tr>
 				<td class="caption"><label for="name">Name</label></td>
 				<td class="input">
-					<input id="name" type="text" name="name" value="<?php echo $m->getName() ?>" 
+					<input id="name" type="text" name="name" value="<?php echo $m->getName() ?>"
 					maxlength="<?php echo $m->inspectName('max_length') ?>" />
 				</td>
 			</tr>
-			
+
 			<tr>
 				<td class="caption"><label for="colour">Colour</label></td>
 				<td class="input">
@@ -40,20 +41,29 @@ $tpl->place('header');
 					<input type="checkbox" id="colour" name="colour" value="1" <?php fHTML::showChecked($m->getColour(), 1) ?> />
 				</td>
 			</tr>
-			
+
 			<tr>
 				<td>&nbsp;</td>
 				<td>
-					<input type="submit" class="btn btn_pos" 
-					value="<?php echo ($action == 'add') ? 'Add' : 'Save'; ?>" />
+					<button type="submit" name="next" class="btn btn_pos" value="index"><?php echo ($action == 'add') ? 'Add' : 'Save'; ?></button>
+
+					<?php if ($action === 'add'): ?>
+					<button type="submit" name="next" class="btn btn_misc" value="add">Add new + another</button>
+					<?php endif; ?>
 				</td>
 			</tr>
-			
+
 		</table>
-		
+
 	</form>
-	
+
 </div>
+
+<script type="text/javascript">
+$(document).ready(function(){
+	$("input[name='name']").focus();
+});
+</script>
 
 <?php
 $tpl->place('footer');
